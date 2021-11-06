e138d6e744002bdeb0de22ed4ac90a43a98f7e6c
mohith-das <92079599+mohith-das@users.noreply.github.com>
2021-11-07 03:03:41 +0530

    Update txn_generator.js

--- txn_generator.js
+++ txn_generator.js
@@ -1,58 +1,65 @@
 function createTxn() {
-    const subDetailSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Subscription Details")
-    const txnDetailSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Transactions")
-    const subData = subDetailSheet.getRange(2, 1, subDetailSheet.getLastRow() - 1, subDetailSheet.getLastColumn()).getDisplayValues()
-    const txnData = txnDetailSheet.getRange(3, 1, txnDetailSheet.getLastRow() - 1, txnDetailSheet.getLastColumn()).getDisplayValues()
-    const currentDate = new Date()
+   const subDetailSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Subscription Details")
+   const txnDetailSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Transactions")
+   const subData = subDetailSheet.getRange(2,1,subDetailSheet.getLastRow()-1,subDetailSheet.getLastColumn()).getDisplayValues()
+   const txnData = txnDetailSheet.getRange(3,1, txnDetailSheet.getLastRow()-1,txnDetailSheet.getLastColumn()).getDisplayValues()
+   const currentDate = new Date()
+   //let errors = []
+
+  subData.forEach(row =>{
+    console.log("in createtxn foreach",row)
+    try{
+      //SubID[0] - Company ID[2] - Invoice Date(Upcoming)[6] => txnNew
+      var res = checkTxn((row[0] + "/" + row[2] + "/" + new Date(row[6]).getTime().toString()).trim(), txnData)
+      if(res.length === 0){
+        console.log("In createtxn if condition",row)
+
+        var writeData = [row[0],row[1],row[2],"","","",row[3],row[4],row[6],row[7],row[8],row[9],row[10],row[11],row[12],row[13]]
+        txnDetailSheet.getRange(txnDetailSheet.getLastRow() + 1, 2, 1, writeData.length).setValues([writeData])
+      }
+
+  }catch(err){
+    console.log(err)
+    //errors.push(["Failed"])
+  }
+  });
 
 
-    let errors = []
-    //txnData.getRange("H3").setFormula("=3+5");
-    subData.forEach(row => {
-        try {
-            var res = checkTxn((row[0] + "/" + row[2] + "/" + new Date(row[5]).getTime().toString()).trim(), txnData)
-            if (res.length === 0) {
-                var writeData = [row[5], row[2], row[1], row[0], row[8], row[6]]
-                txnDetailSheet.getRange(txnDetailSheet.getLastRow() + 1, 2, 1, 6).setValues([writeData])
-            }
+applyFormulaFunc(txnDetailSheet,1,"A3",`=CONCAT("EDLR/INV/",Row()-2)`) // Gen Invoice Number
+applyFormulaFunc(txnDetailSheet,5,"E3",`=VLOOKUP($D3,'Company Details'!$A$2:$L,3,False)`) //Get POC 
+applyFormulaFunc(txnDetailSheet,6,"F3",`=VLOOKUP($D3,'Company Details'!$A$2:$L,5,False)`) //Get Address
+applyFormulaFunc(txnDetailSheet,7,"G3",`=VLOOKUP($D3,'Company Details'!$A$2:$L,7,False)`) //Get Merchant GST
+applyFormulaFunc(txnDetailSheet,18,"R3",`=MONEYTEXT(Q3,"INR")`) // Amount to Text
+applyFormulaFunc(txnDetailSheet,20,"T3",`=VLOOKUP($D3,'Company Details'!$A$2:$L,10,False)`)//Get Recipient
+applyFormulaFunc(txnDetailSheet,21,"U3",`=VLOOKUP($D3,'Company Details'!$A$2:$L,11,False)`)//Get CC
 
-        } catch (err) {
-            console.log(err)
-            // errors.push(["Failed"])
-        }
-    });
-
-    //Generating Invoice Number and Emails 
-    applyFormulaFunc(txnDetailSheet, 1, "A3", `=CONCAT("EDLR/INV/",Row()-2)`)
-    applyFormulaFunc(txnDetailSheet, 8, "H3", `=VLOOKUP($C3,'Contact Info'!$A$2:$G$1001,7,False)`)
-    applyFormulaFunc(txnDetailSheet, 9, "I3", `=VLOOKUP($E3,'Subscription Details'!$A$2:$J$1001,10,False)`)
-    applyFormulaFunc(txnDetailSheet, 10, "J3", `=VLOOKUP($C3,'Company Details'!$A$2:$G$1001,5,False)`)
-    applyFormulaFunc(txnDetailSheet, 11, "K3", `=MONEYTEXT(G3,"INR")`)
-    applyFormulaFunc(txnDetailSheet, 12, "L3", `=VLOOKUP($E3,'Subscription Details'!$A$2:$J$1001,4,False)`)
-
-
-    //currentSheet.getRange(2,3,currentSheet.getLastRow()-1,1).setValues(errors)
+  //currentSheet.getRange(2,3,currentSheet.getLastRow()-1,1).setValues(errors)
 }
 
-function checkTxn(txnNew, txnData) {
-    let match = []
-    txnData.forEach(txn_row => {
+function checkTxn(txnNew,txnData)
+{ 
+  
 
-        if (txnNew === (txn_row[4] + "/" + txn_row[2] + "/" + new Date(txn_row[1]).getTime().toString()).trim()) {
-            match.push(txnNew);
-        }
+  let match = []
+txnData.forEach(txn_row=>{
+   //SubID[0] - Company ID[2] - Invoice Date(Upcoming)[6] => txnNew
+   //SubID[1] - Company ID[3] - Invoice Date(Old)[9] => txnData old
+  if(txnNew === (txn_row[1] + "/" + txn_row[3] + "/" + new Date(txn_row[9]).getTime().toString()).trim())
+  {
+    match.push(txnNew);
+  }
 
-    })
-    return match;
+})
+  return match;
 }
 
 
-function applyFormulaFunc(sheet, col, cell, formula) {
-    //const txnDetailSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Transactions");
-
-    sheet.getRange(cell).setFormula(formula);
-    const lr = sheet.getLastRow();
-    const fillDownRange = sheet.getRange(3, col, lr - 2, 1);
-    sheet.getRange(cell).copyTo(fillDownRange);
+function applyFormulaFunc(sheet,col, cell, formula)
+{
+   
+  sheet.getRange(cell).setFormula(formula);
+  const lr = sheet.getLastRow();
+  const fillDownRange = sheet.getRange(3,col,lr-2,1);
+  sheet.getRange(cell).copyTo(fillDownRange);
 
 }
